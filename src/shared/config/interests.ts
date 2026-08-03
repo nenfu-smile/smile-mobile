11081d58dcdc11a42ee48eb7c4b3fbdb3aab16db
Arome8240 <arome8240@gmail.com>
2026-08-03 16:46:19 +0100

    feat: add INTERESTS configuration with various activity categories and icons

---
+++ src/shared/config/interests.ts
@@ -0,0 +1,10 @@
+import { Activity, Buy, Category, Image2, TicketStar, TwoUsers } from "react-native-iconly";
+
+export const INTERESTS = [
+  { key: "socializing", label: "Socializing", Icon: TwoUsers },
+  { key: "art-culture", label: "Art & Culture", Icon: Image2 },
+  { key: "food-drink", label: "Food & Drink", Icon: Buy },
+  { key: "tech-meetups", label: "Tech Meetups", Icon: Category },
+  { key: "concerts", label: "Concerts", Icon: TicketStar },
+  { key: "fitness-sports", label: "Fitness & Sports", Icon: Activity },
+];
