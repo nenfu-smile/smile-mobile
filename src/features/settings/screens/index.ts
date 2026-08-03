3bb41c777974305e418c3d6bd5c2aef8e998ef90
Arome8240 <arome8240@gmail.com>
2026-08-03 16:58:47 +0100

    feat: add index file for settings screens exports

---
+++ src/features/settings/screens/index.ts
@@ -0,0 +1,5 @@
+export { SettingsScreen } from "./SettingsScreen";
+export { DevicesScreen } from "./DevicesScreen";
+export { NotificationSettingsScreen } from "./NotificationSettingsScreen";
+export { DeleteAccountScreen } from "./DeleteAccountScreen";
+export { UpdateProfileScreen } from "./UpdateProfileScreen";
